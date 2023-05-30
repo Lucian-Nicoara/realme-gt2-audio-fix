f5bde112a4eb0e9e8d7ae3c52f0ba8e2f1a3ef5f
Lucian Nicoara <mail.lucian@gmail.com>
2023-05-30 22:02:26 +0300

    Updated recording profiles for other scenarios

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -10,4 +10,10 @@ chown root:root /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/*
 chmod 0644 /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/*
 chcon -v u:object_r:vendor_configs_file:s0 /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/*
 
+mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID1_MIC_Normal.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID1_MIC_Normal.txt
 mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID4_MIC_Cam.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID4_MIC_Cam.txt
+mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID5_MIC_Conference.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID5_MIC_Conference.txt
+mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID6_MIC_Fast.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID6_MIC_Fast.txt
+mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID7_MIC_Interview.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID7_MIC_Interview.txt
+mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID8_MIC_Unprocess.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID8_MIC_Unprocess.txt
+mount -o bind /data/adb/modules/GT2RecQualityMod/odm-files/lvimfs_params/LVIMFS_Parameter_ID9_MIC_Voice.txt /odm/etc/lvimfs_params/LVIMFS_Parameter_ID9_MIC_Voice.txt
